0ddf3aa85c4f6dd2c32957c90f085e1d1b6fb4bd
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2023-07-26 23:54:09 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (83.02%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,73 +1,58 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) 
     {
-        int a[]=new int [2];
-        int n = nums.length;
-//         int first = 0, last = n-1;
-//         int ans = -1;
-//         int mid = first + (last - first)/2;
-//         while(first < last)
-//         {
-//             if(nums[mid] == target)
-//             {
-//                 ans = mid;
-//                 break;
-//             }
-//             else if(nums[mid] > target)
-//             {
-//                 last = mid - 1;
-//             }
-//             else
-//             {
-//                 last = mid + 1;
-//             }
-//             mid = first + (last - first)/2;
-//         }
-//         if(ans==-1)
-//         {
-//             a[0]=-1;
-//             a[1]=-1;
-//             return a; 
-//         }
-//         int start = mid;
-//         int end = mid;
-//         while(nums[start]==target)
-//         {
-//             start--;
-//             if(start==-1)
-//                 break;
-//         }
-//         start+=1;
-//         while(nums[end]==target)
-//         {
-//             end++;
-//             if(end == n)
-//                 break;
-//         }
-//         end-=1;
-//         a[0]=start;
-//         a[1]=end;
-//         return a;
-         a[0]=-1;
-         a[1]=-1;
-        for(int i=0,j=n-1;i<=j;)
-        {
-            if(nums[i] == target)
-            {
-                a[0] = i;
-            }
+        // int a[]=new int [2];
+        // int n = nums.length;
+        //  a[0]=-1;
+        //  a[1]=-1;
+        // for(int i=0,j=n-1;i<=j;)
+        // {
+        //     if(nums[i] == target)
+        //     {
+        //         a[0] = i;
+        //     }
+        //     else
+        //         i++;
+        //     if(nums[j] == target)
+        //     {
+        //         a[1] = j;
+        //     }
+        //     else
+        //         j--;
+        //     if(a[0]!=-1&&a[1]!=-1)
+        //         break;
+        // }
+        // return a;
+        
+        int ans[]={-1,-1};
+        int start=search(nums,target,true);
+        int end=search(nums,target,false);
+        ans[0]=start;
+        ans[1]=end; 
+        return ans;
+    }
+    int search(int nums[],int target,boolean findstartindex){
+        int ans=-1;
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int middle=(start+end)/2;
+            if(target<nums[middle])
+                end=middle-1;
+            else if(target>nums[middle])
+                start=middle+1;
             else
-                i++;
-            if(nums[j] == target)
             {
-                a[1] = j;
+                ans=middle;
+                if(findstartindex == true){
+                    end=middle-1;
+                }
+                else{
+                    start=middle+1;
+                }
             }
-            else
-                j--;
-            if(a[0]!=-1&&a[1]!=-1)
-                break;
-        }
-        return a;
+       }
+        return ans;
+    }
     }
    
-}
