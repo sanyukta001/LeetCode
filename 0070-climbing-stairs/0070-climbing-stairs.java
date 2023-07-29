82c1fce44abab2f701c90e3e07d836aaaecf7fdd
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2023-07-29 23:40:42 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (39.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -17,13 +17,7 @@ class Solution
             arr[x] = arr[x-1] + arr[x-2];
             return arr[x];
         }
-        // else
-        // // {
-        // //     if(arr[x-1] == -1)
-        // //         fibo(x-1,arr);
-        // //     if(arr[x-2] == -1)
-        // //         fibo(x-2,arr);
-        // // }
+        
         return fibo(x-1,arr)+fibo(x-2,arr);
     }
     public int climbStairs(int n) 
@@ -31,8 +25,5 @@ class Solution
         int arr[] = new int[n+2];
         Arrays.fill(arr,-1);
         return fibo(n+1,arr)-1;
-        // if((n == 0 ) || (n == 1))
-        //     return 1;
-        // return climbStairs(n-1)+climbStairs(n-2);
     }
 }
