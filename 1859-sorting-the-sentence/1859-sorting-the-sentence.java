4ee3c71f3065ca8483d41e8b9ed794c710f6fa2f
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2023-08-19 19:54:57 +0530

    Time: 4 ms (21.80%), Space: 40.7 MB (30.73%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String sortSentence(String s) {
+        String[] arr = new String [10];
+        int c = 0;
+        s=s+" ";
+        int p = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)==' ')
+            {
+                int x=(s.charAt(i-1)-'0');
+                arr[x] = s.substring(p,i-1);
+                p = i+1;
+                c++;
+            }
+        }
+        String w = "";
+        for(int i=1;i<=c;i++)
+        {
+            if(i==c)
+                w = w + arr[i];
+            else
+                w = w + arr[i]+" ";
+        }
+        return w;
+    }
+}
