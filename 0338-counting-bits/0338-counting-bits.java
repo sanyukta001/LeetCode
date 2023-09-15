14eef8c98e8dee4e310e264ba1f5ab86f306c908
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2023-09-15 21:00:38 +0530

    Time: 10 ms (12.08%), Space: 46.2 MB (85.95%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -6,32 +6,19 @@ class Solution {
         for(int i = 1; i <= n;i++)
         {
             int bin = binary(i);
-            // while(bin>0)
-            // {
-            //     b = bin%10;
-            //     if(b == 1)
-            //         count++;
-            //     bin = bin/10;
-            // }
-            // ans[i] = count;
-                ans[i] = bin;
+            ans[i] = bin;
         }
         return ans;
     }
     public int binary(int n)
     {
-        String s = "";
-        // int b = 0;
-                int x = 0;
+        int x = 0;
         while(n>0)
         {
-            // b = n%2;
             if((n%2)==1)
                 x++;
-            //s =s+(""+b);
             n = n/2;
         }
-        //int l = Integer.parseInt(s);
         return x;
     }
 }
