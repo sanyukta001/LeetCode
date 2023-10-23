105eec420a2e83f6cbf04e476a51fbe784448503
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2023-10-23 15:03:05 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (73.73%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,14 @@
 class Solution {
     public double myPow(double x, int n) {
         return Math.pow(x,n);
+        // double res = 1.0;
+        // int c = 0;
+        // while(c<n)
+        // {
+        //     res = res*x;
+        //     c++;
+        // }
+        // return res;
     }
 }
 //=nb
