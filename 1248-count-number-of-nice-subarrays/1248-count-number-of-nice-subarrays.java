27605d452f9d6ba252470174a9fc15d7e397720e
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2024-01-14 11:08:50 +0530

    Time: 48 ms (7.58%), Space: 55.5 MB (19.33%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        // List<Integer> list = new ArrayList<>();
+        // int niceCount = 0;
+        // for(int i = 0; i < nums.length; i++)
+        // {
+        //     for(int j = i; j < nums.length; j++)
+        //     {
+        //         if(j-i+1 >= k)
+        //         {
+        //             int oddCount = 0;
+        //             int x = i;
+        //             while(x<=j)
+        //             {
+        //                 if(nums[x]%2!=0)
+        //                     oddCount++;
+        //                 x++;
+        //             }
+        //             if(oddCount == k){
+        //                 niceCount++;
+        //                 break;
+        //             }
+        //         }
+        //     }
+        // }
+        // return niceCount;
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i]%2 == 0)
+                nums[i] = 0;
+            else
+                nums[i] = 1;
+        }
+        Map<Integer,Integer> map = new HashMap<>();
+        int sum = 0;
+        int niceCount = 0;
+        // map.put(0,1);
+        for(int i = 0; i < nums.length; i++)
+        {
+            sum += nums[i];
+            if(sum==k)
+                niceCount++;
+            if(map.containsKey(sum-k))
+            {
+                niceCount+=map.get(sum-k);
+                // map.put(sum-k,map.get(sum-k)+1);
+            }
+            if(!map.containsKey(sum))
+                map.put(sum,1);
+            else
+                map.put(sum,map.get(sum)+1);
+        }
+        return niceCount;
+    }
+}
+//nb=
