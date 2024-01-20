6038c71bfcf2b478fc1c60aa5136e76f5c7dc8b7
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2024-01-20 14:51:36 +0530

    Time: 848 ms (5.09%), Space: 50.1 MB (30.77%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,12 +1,10 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int mi = Integer.MAX_VALUE;
         long sum = 0;
         for(int i = 0; i<arr.length; i++)
         {
             int j = i;
             int k = i;
-            // mi = arr[i];
             int g2 = 1, g1 = 1;
             while(j+1 < arr.length && arr[j+1]>arr[i])
             {
@@ -21,17 +19,6 @@ class Solution {
             long prod = (g1*g2)%1000000007;
             prod = (prod*arr[i])%1000000007;
             sum = (sum%1000000007+prod)%1000000007;
-            // while(j<arr.length)
-            // {
-            //     if(arr[j]<mi)
-            //     {
-            //         mi = arr[j];
-            //         // sum = sum + mi;
-            //     }
-            //     // else
-            //         sum += mi;
-            //     j++;
-            // }
         }
         return (int)sum;
     }
