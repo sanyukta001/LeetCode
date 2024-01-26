ff96e5f4057efba67af79476ebf641e4d5824e60
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2024-01-26 19:29:13 +0530

    Time: 19 ms (10.80%), Space: 45 MB (7.60%) - LeetHub

---
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.java
@@ -0,0 +1,49 @@
+class Solution {
+    int mod = 1000000007;
+    int[][][]dp;
+    public int findPaths(int m, int n, int maxMove, int i, int j) {
+        dp = new int[51][51][51];
+        for (int deck = 0; deck < dp.length; deck++) 
+        {
+            for (int row = 0; row < dp[deck].length; row++) 
+            {
+                for (int col = 0; col < dp[deck][row].length; col++) 
+                {
+                    dp[deck][row][col] = -1;
+                }
+            }
+        }
+
+        return (int)(findP(m,n,maxMove,i,j)%mod);
+    }
+    long findP(int m, int n, int maxMove, int i, int j){
+        long count = 0;
+        if(i<0||i>=m||j<0||j>=n)
+            return 1;
+        if(maxMove <= 0)
+            return 0;
+            // if(i+1 < 0 || i+1 >= m || j < 0 || j >= n)
+            //     count++;
+            // if(i-1 < 0 || i-1 >= m|| j < 0 || j >= n)
+            //     count++;
+            // if(i < 0 || i >= m || j-1 < 0 || j-1 >= n)
+            //     count++;
+            // if(i < 0 || i >= m || j +1 < 0 || j +1 >= n)
+            //     count++;
+            // return count;
+            // if((startRow < 0 || startRow >= m || startColumn < 0 || startColumn >= n))
+            // {
+            //     return 1;
+            // }
+            // return 0;
+        if(dp[i][j][maxMove] != -1)
+            return dp[i][j][maxMove];
+        count += findP(m,n,maxMove-1,i-1,j);
+        count += findP(m,n,maxMove-1,i+1,j);
+        count += findP(m,n,maxMove-1,i,j+1);
+        count += findP(m,n,maxMove-1,i,j-1);
+        dp[i][j][maxMove] = (int)(count%mod);
+        return dp[i][j][maxMove];    
+    }
+}
+//nb=
