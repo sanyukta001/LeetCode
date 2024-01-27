99f259ee6dc07227369fef3d53c332aa5fede087
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2024-01-27 18:13:37 +0530

    Time: 24 ms (70.13%), Space: 47.4 MB (6.32%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,21 +1,42 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int i = 0, j = 0;
-        int count = 0;
-        int sum = 0;
-        while(i<nums.length)
-        {
-            sum += nums[j];
-            j++;
-            if(sum == k)
-                count++;
-            if(j == nums.length)
-            {
-                i++;
-                j = i;
-                sum = 0;
-            }
+//         int i = 0, j = 0;
+//         int count = 0;
+//         int sum = 0;
+//         while(i<nums.length)
+//         {
+//             sum += nums[j];
+//             j++;
+//             if(sum == k)
+//                 count++;
+//             if(j == nums.length)
+//             {
+//                 i++;
+//                 j = i;
+//                 sum = 0;
+//             }
             
+//         }
+//         return count;
+        
+        // HashSet<Integer> set = new HashSet();
+        Map<Integer,Integer> set = new HashMap<>();
+        // set.add(0);
+        set.put(0,1);
+        int prefixSum = 0;
+        int count=0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            prefixSum+=nums[i];
+            // if(set.contains(prefixSum-k))
+            //     count++;
+            // set.add(prefixSum);
+            if(set.containsKey(prefixSum-k))
+                count+=set.get(prefixSum-k);
+            if(!set.containsKey(prefixSum))
+                set.put(prefixSum,1);
+            else
+                set.put(prefixSum,set.get(prefixSum)+1);
         }
         return count;
     }
