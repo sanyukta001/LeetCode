2e920b490c6215011c9c64f5496b891279c11971
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2024-01-27 13:53:49 +0530

    Time: 19 ms (54.62%), Space: 56.3 MB (15.97%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.java
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.java
@@ -1,9 +1,6 @@
 class Solution {
     int[][] dp = new int[1001][1001];
     public int kInversePairs(int n, int k) {
-        // int[][]dp = new int[1001][1001];
-        // for(int[] a: dp)
-        //     Arrays.fill(a,1);
         dp[0][0] = 1;
         for(int i = 1; i <= n; i++)
         {
@@ -14,30 +11,9 @@ class Solution {
                 if(j>=i)
                     val-=dp[i-1][j-i];
                 dp[i][j] = (int) (val%1000000007);
-                // for(int i = 0; i <= Math.min(y,x-1);i++)
-                // {
-                //     dp[x][y] = (dp[x][y]+dp[x-1][y-i])%1000000007;
-                // }
             }
         }
         return dp[n][k];
     }
-    // int kInverse(int n, int k){
-    //     if(n == 0)
-    //         return 0;
-    //     if(k == 0)
-    //         return 1;
-    //     if(dp[n][k]!=-1)
-    //         return dp[n][k];
-    //     else{
-    //     dp[]
-    //     for(int i = 0; i <= Math.min(k,n-1); i++)
-    //     {
-    //         c=(c+kInverse(n-1,k-i))%1000000007;
-    //     }
-    //     dp[n][k] = (int)c%1000000007;
-    //     return dp[n][k];
-    //     }
-    // }
 }
 //nb=
