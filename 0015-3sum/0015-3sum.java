47d682ce7c31cee0e35aa6332f25079bd27c5a45
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2024-01-28 14:14:28 +0530

    Time: 48 ms (40.39%), Space: 52.3 MB (19.81%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,63 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+//         List<List<Integer>> res = new ArrayList();
+//         HashSet<List<Integer>> set = new HashSet();
+//         for(int i = 0; i < nums.length; i++)
+//         {
+//             HashSet<Integer> check = new HashSet();
+//             for(int j = i+1; j < nums.length; j++)
+//             {
+//                 List<Integer> temp = new ArrayList();
+//                 if(check.contains(0-(nums[i]+nums[j])))
+//                 {
+//                     // temp = [nums[i],nums[j],(0-(nums[i]+nums[j]))];
+//                     temp.add(nums[i]);
+//                     temp.add(nums[j]);
+//                     temp.add(0-(nums[i]+nums[j]));
+//                     Collections.sort(temp);
+//                     set.add(temp);
+//                 }
+//                 check.add(nums[j]);
+                
+//             }
+//         }
+//         return new ArrayList(set);
         List<List<Integer>> res = new ArrayList();
-        HashSet<List<Integer>> set = new HashSet();
-        for(int i = 0; i < nums.length; i++)
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length;i++)
         {
-            HashSet<Integer> check = new HashSet();
-            for(int j = i+1; j < nums.length; j++)
+            if(i!=0 && nums[i] == nums[i-1])
+                continue;
+            int j = i+1;
+            int k = nums.length-1;
+            while(j<k)
             {
-                List<Integer> temp = new ArrayList();
-                if(check.contains(0-(nums[i]+nums[j])))
+                while(j!=i+1 && j<nums.length && nums[j] == nums[j-1])
+                    j++;
+                while(k!=nums.length-1 && k>=0 && nums[k] == nums[k+1])
+                    k--;
+                if(j>=k)
+                    break;
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum==0)
                 {
-                    // temp = [nums[i],nums[j],(0-(nums[i]+nums[j]))];
+                    List<Integer> temp = new ArrayList();
                     temp.add(nums[i]);
                     temp.add(nums[j]);
-                    temp.add(0-(nums[i]+nums[j]));
-                    Collections.sort(temp);
-                    set.add(temp);
+                    temp.add(nums[k]);
+                    res.add(temp);
+                    j++;
+                    k--;
                 }
-                check.add(nums[j]);
-                
+                else if(sum<0)
+                {
+                    j++;
+                }
+                else
+                    k--;
             }
         }
-        return new ArrayList(set);
+        return res;
     }
 }
 //nb=
