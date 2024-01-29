03a5302ee1df3fbd02832406062bdb918fa3fbda
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2024-01-30 00:54:39 +0530

    Time: 15 ms (74.94%), Space: 44.4 MB (38.20%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -25,34 +25,85 @@ class Solution {
         //     l++;
         // }
         // return arr;
+        // List<List<Integer>> res = new ArrayList();
+        // HashSet<List<Integer>> set = new HashSet();
+        // for(int i = 0; i < nums.length; i++)
+        // {
+        //     for(int j = i+1; j < nums.length; j++)
+        //     {
+        //         HashSet<Long> check = new HashSet();
+        //         for(int k = j+1; k < nums.length; k++)
+        //         {
+        //             // HashSet<Integer> check = new HashSet();
+        //             long sum = nums[i]+nums[j];
+        //             sum+=nums[k];
+        //             long fourth = target- sum;
+        //             if(check.contains(fourth))
+        //             {
+        //                 List<Integer> temp = new ArrayList();
+        //                 temp.add(nums[i]);
+        //                 temp.add(nums[j]);
+        //                 temp.add(nums[k]);
+        //                 temp.add((int)fourth);
+        //                 Collections.sort(temp);
+        //                 set.add(temp);
+        //             }
+        //                 check.add((long)nums[k]);
+        //         }
+        //     }
+        // }
+        // return new ArrayList(set);
+        Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList();
-        HashSet<List<Integer>> set = new HashSet();
         for(int i = 0; i < nums.length; i++)
         {
+            if(i!=0 && nums[i] == nums[i-1])
+                continue;
             for(int j = i+1; j < nums.length; j++)
             {
-                HashSet<Long> check = new HashSet();
-                for(int k = j+1; k < nums.length; k++)
+                if(j != i+1 && nums[j] == nums[j-1])
+                    continue;
+                    
+                int k = j+1;
+                int l = nums.length-1;
+                // int sum = 0;
+                while(k<l)
                 {
-                    // HashSet<Integer> check = new HashSet();
-                    long sum = nums[i]+nums[j];
+                    
+                    // if(k>=l)
+                    //     break;
+                    long sum = nums[i];
+                    sum+=nums[j];
                     sum+=nums[k];
-                    long fourth = target- sum;
-                    if(check.contains(fourth))
+                    sum+=nums[l];
+                    if(sum == target)
                     {
                         List<Integer> temp = new ArrayList();
                         temp.add(nums[i]);
                         temp.add(nums[j]);
                         temp.add(nums[k]);
-                        temp.add((int)fourth);
-                        Collections.sort(temp);
-                        set.add(temp);
+                        temp.add(nums[l]);
+                        res.add(temp);
+                        k++;
+                        l--;
+                        while( k<l && nums[k-1]==nums[k])
+                            k++;
+                        while(k<l && nums[l+1] == nums[l])
+                            l--;
                     }
-                        check.add((long)nums[k]);
+                    else if((int)sum > target)
+                    {
+                        l--;
+                    }
+                    else
+                    {
+                        k++;
+                    }
+                    
                 }
             }
         }
-        return new ArrayList(set);
+        return res;
     }
 }
 //=nb=
