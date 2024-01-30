3aae347eef9319e9fc32cd3cf9089fd305dfefad
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2024-01-30 13:07:46 +0530

    Time: 3 ms (64.07%), Space: 44.5 MB (37.96%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,64 +1,5 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-//         for(int i = 0; i < 9;i++)
-//         {
-//             for(int j = 0; j < 9; j++)
-//             {
-//                 HashMap<Character, Integer> col = new HashMap<>();
-//                 HashMap<Character,Integer> row = new HashMap<>();
-//                 char c = board[i][j];
-//                 char r = board[j][i];
-//                     if(c!='.')
-//                     {
-//                         if(col.containsKey(c))
-//                             return false;
-//                         else
-//                             col.put(c,1);
-//                     }
-//                     if(r!='.')
-//                     {
-//                         if(row.containsKey(r))
-//                             return false;
-//                         else
-//                             row.put(r,1);
-//                     }
-                
-//                 }
-//         }
-//             int si=0, ei=2, sj=0, ej=2;
-//             int count =0;
-//         while(count != 9)
-//         {
-//             count++;
-//             HashMap<Character,Integer> check = new HashMap<>();
-//             for(int i = si; i <=ei;i++)
-//             {
-//                 for(int j =sj;j<=ej;j++)
-//                 {
-//                     char x = board[i][j];
-//                     if(x!='.')
-//                     {
-//                         if(check.containsKey(x))
-//                             return false;
-//                         else
-//                             check.put(x,1);
-//                     }
-//                 }
-//             }
-//             if(ei == 8)
-//             {
-//                 sj = sj + 3;
-//                 ej = ej + 3;
-//                 si = 0;
-//                 ei = 2;
-//             }
-//             else
-//             {
-//             si = si + 3;
-//             ei = ei + 3;
-//             }
-//         }
-//         return true;
         
         for(int i = 0; i < 9;  i++)
         {
@@ -84,7 +25,6 @@ class Solution {
         while(rowStart < 7 && rowEnd < 9)
         {
             int columnStart = 0 ,columnEnd = 2;
-            // HashSet<Character> gmap = new HashSet<>();
             while(columnStart < 7 && columnEnd < 9)
             {
                 HashSet<Character> gmap = new HashSet<>();
