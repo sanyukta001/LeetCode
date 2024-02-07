2f9c91b2d2b1a3fe041b28ab697414392dd25150
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2024-02-07 19:35:20 +0530

    Time: 208 ms (17.75%), Space: 46.8 MB (5.40%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,36 +1,28 @@
 class Solution {
-    public static <K, V extends Comparable<V> > Map<K, V> 
-    valueSort(final Map<K, V> map) 
-    { 
-        // Static Method with return type Map and 
-        // extending comparator class which compares values 
-        // associated with two keys 
-        Comparator<K> valueComparator = new Comparator<K>() { 
+//     public static <K, V extends Comparable<V> > Map<K, V> 
+//     valueSort(final Map<K, V> map) 
+//     { 
+//         Comparator<K> valueComparator = new Comparator<K>() { 
+//                   public int compare(K k1, K k2) 
+//                   { 
+//                       int comp = map.get(k1).compareTo( 
+//                           map.get(k2)); 
+//                       if (comp == 0) 
+//                           return 1; 
+//                       else
+//                           return comp; 
+//                   } 
             
-                  // return comparison results of values of 
-                  // two keys 
-                  public int compare(K k1, K k2) 
-                  { 
-                      int comp = map.get(k1).compareTo( 
-                          map.get(k2)); 
-                      if (comp == 0) 
-                          return 1; 
-                      else
-                          return comp; 
-                  } 
-            
-              }; 
+//               }; 
+//         Map<K, V> sorted = new TreeMap<K, V>(valueComparator); 
         
-        // SortedMap created using the comparator 
-        Map<K, V> sorted = new TreeMap<K, V>(valueComparator); 
+//         sorted.putAll(map); 
         
-        sorted.putAll(map); 
-        
-        return sorted; 
-    } 
+//         return sorted; 
+//     } 
     public String frequencySort(String s) {
         Map<Character,Integer> map = new TreeMap<>();
-        String res = "";
+        // String res = "";
         for(int i = 0; i < s.length(); i++)
         {
             char x = s.charAt(i);
@@ -39,22 +31,45 @@ class Solution {
             else
                 map.put(x,map.get(x)+1);
         }
-        Map sortedMap = valueSort(map);
-        Set set = sortedMap.entrySet(); 
-        Iterator i = set.iterator(); 
-        while (i.hasNext())
+        String[] res = new String[s.length()+1];
+        Arrays.fill(res,"");
+        for(Map.Entry<Character,Integer> mp:map.entrySet())
         {
-            Map.Entry mp = (Map.Entry)i.next();
-            // char x = mp.getKey(); 
-            // int y = mp.getValue();
-            String temp = "";
-            for(int y = 1; y <= (int) mp.getValue(); y++)
-            {
-                temp = temp + mp.getKey();
-            }
-            res =  temp+res;
+            int y = mp.getValue();
+            char x = mp.getKey();
+            res[y] += x; 
         }
-        return res;
+        String fa = "";
+        for(int i = 1; i < s.length()+1; i++)
+        {
+            if(res[i] != "")
+            {
+                for(int j = 0; j < res[i].length(); j++)
+                {
+                    String temp = "";
+                    for(int k = 1; k <= i; k++)
+                    {
+                        temp += res[i].charAt(j); 
+                    }
+                    fa = temp + fa;
+                }
+            }
+        }
+        return fa;
+        // Map sortedMap = valueSort(map);
+        // Set set = sortedMap.entrySet(); 
+        // Iterator i = set.iterator(); 
+        // while (i.hasNext())
+        // {
+        //     Map.Entry mp = (Map.Entry)i.next();
+        //     String temp = "";
+        //     for(int y = 1; y <= (int) mp.getValue(); y++)
+        //     {
+        //         temp = temp + mp.getKey();
+        //     }
+        //     res =  temp+res;
+        // }
+        // return res;
     }
 }
 //nb=
