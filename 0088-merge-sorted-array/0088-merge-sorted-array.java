678377ec1fa0b9def3af8b6ca4d34b702787448e
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2024-02-16 22:08:34 +0530

    Time: 1 ms (35.65%), Space: 41.9 MB (86.00%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,27 +1,55 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int nums3[] = new int[m+n]; 
-        int k = 0;
-        int i = 0;
-        int j = 0;
-        while(i<m && j<n)
+        // int nums3[] = new int[m+n]; 
+        // int k = 0;
+        // int i = 0;
+        // int j = 0;
+        // while(i<m && j<n)
+        // {
+        //     if(nums1[i] <= nums2[j])
+        //         nums3[k++] = nums1[i++];
+        //     else
+        //         nums3[k++] = nums2[j++];
+        // }
+        // while(i<m)
+        // {
+        //     nums3[k++] = nums1[i++];
+        // }
+        // while(j<n)
+        // {
+        //     nums3[k++] = nums2[j++];
+        // }
+        // for(int x = 0; x < k; x++)
+        // {
+        //     nums1[x] = nums3[x];
+        // }
+        
+        int i = 0, j = 0;
+        while(i<m+n&&j < n)
         {
-            if(nums1[i] <= nums2[j])
-                nums3[k++] = nums1[i++];
+            // if(nums1[i] == 0)
+            //     break;
+            if(nums1[i] < nums2[j])
+                i++;
             else
-                nums3[k++] = nums2[j++];
+            {
+                for(int k = m+n-1; k > i; k--)
+                    nums1[k] = nums1[k-1];
+                nums1[i] = nums2[j];
+                j++;
+                i++;
+            }
         }
-        while(i<m)
-        {
-            nums3[k++] = nums1[i++];
-        }
+        int x = n-1;
+        int y = nums1.length-1;
         while(j<n)
         {
-            nums3[k++] = nums2[j++];
+            nums1[y]=nums2[x];
+            j++;
+            y--;
+            x--;
         }
-        for(int x = 0; x < k; x++)
-        {
-            nums1[x] = nums3[x];
-        }
+    
     }
 }
+//nb=
