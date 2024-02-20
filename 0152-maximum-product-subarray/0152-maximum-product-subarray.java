6a35d27e81c50f8fc7f889b00bf4dc88d315b977
Sanyukta Mishra <96128191+sanyukta001@users.noreply.github.com>
2024-02-20 20:05:30 +0530

    Time: 1 ms (89.69%), Space: 44.4 MB (92.92%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,22 +1,6 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        // int l = nums.length;
-        // int maxProd = Integer.MIN_VALUE;
-        // int prod = 1;
-        // for(int i = 0; i < l; i++)
-        // {
-        //     prod = nums[i];
-        //     maxProd = Math.max(maxProd,prod);
-        //     for(int j = i+1; j < l; j++)
-        //     {
-        //         prod *= nums[j];
-        //         maxProd = Math.max(maxProd, prod);
-        //     }
-        // }
-        // return maxProd;
-        
         int l = nums.length;
-        // int i = 0;
         int prefix = 1;
         int suffix = 1;
         int maxProd = Integer.MIN_VALUE;
